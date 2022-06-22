711064b8d4834b58f7abcbc2210b4a2742ec93bc
ixia <biplab.mal@keysight.com>
2022-06-22 18:33:23 +0530

    setup kind cluster

--- cmd/cluster/setup.go
+++ cmd/cluster/setup.go
@@ -1,6 +1,7 @@
 package cluster
 
 import (
+	"github.com/open-traffic-generator/ixops/internal/setup"
 	"github.com/spf13/cobra"
 )
 
@@ -8,6 +9,12 @@ var setupCmd = &cobra.Command{
 	Use:   "setup",
 	Short: "Setup cluster",
 	RunE: func(cmd *cobra.Command, args []string) error {
+		// setup.CreateKindConfig()
+		// setup.InstallKind("0.13.0")
+		// setup.CreateKindCluster()
+		// setup.GetKubectl()
+		// setup.GetMetallb()
+		setup.MakeMetallbConfig()
 		return nil
 	},
 }
